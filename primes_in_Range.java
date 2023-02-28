1a025f96710f24e8606fbf7174ec6b761a25841a
Abhishek Behera <96872894+AbhishekBehera20@users.noreply.github.com>
2023-02-28 21:20:23 +0530

    Add files via upload

---
+++ primes_in_Range.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class primes_in_Range {
+    public static boolean isPrime(int n) {
+        if(n == 2) {
+            return true;
+        }
+        for(int i = 2;i<=Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+      public static void primes_in_Range(int n) {
+        for(int i =2;i<=n;i++) {
+            if(isPrime(i)) {
+                System.out.print(i+" ");
+            }
+        }
+        System.out.println();
+      }
+    public static void main(String args[] ) {
+        primes_in_Range(100);
+    }
+}
