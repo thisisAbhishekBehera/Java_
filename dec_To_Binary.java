0faab6f59d08aae188698f0a7dbfae3d91681556
Abhishek Behera <96872894+AbhishekBehera20@users.noreply.github.com>
2023-03-01 18:43:45 +0530

    Add files via upload

---
+++ dec_To_Binary.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class dec_To_Binary {
+    public static void decToBin(int decNum) {
+        int myNum = decNum ;
+        int pow =0;
+        int binNum = 0;
+
+        while(decNum >0) {
+            int rem = decNum % 2;
+            binNum = binNum + (rem * (int)Math.pow(10,pow));
+            pow++;
+            decNum = decNum / 2;
+        }
+        System.out.println("Binary of " + myNum + " = " + binNum );
+
+       
+    }
+
+    public static void main(String args[]) {
+     decToBin(12);
+    }
+}
